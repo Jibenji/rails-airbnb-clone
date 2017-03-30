6a31cf00db220b81a06dc6251445d8f677e5863b
Benjamin Toulemonde <benjamin.toulemonde@ieseg.fr>
2017-03-30 17:24:11 +0200

    sold boolean to artwworks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330151416) do
+ActiveRecord::Schema.define(version: 20170330151906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170330151416) do
     t.string   "photo_artwork"
     t.integer  "price"
     t.string   "title"
+    t.boolean  "sold"
     t.index ["user_id"], name: "index_artworks_on_user_id", using: :btree
   end
 
