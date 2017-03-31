8ad27c26ed71f1eaaef1c73e2b63fe97a73a8f6f
Benjamin Toulemonde <benjamin.toulemonde@ieseg.fr>
2017-03-31 12:19:02 +0200

    change validations on update on users and purchases dependency

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,6 +1,7 @@
 class Artwork < ApplicationRecord
   mount_uploader :photo_artwork, PhotoUploader
   belongs_to :user
+  has_one :purchase, dependent: :destroy
   validates :art_category, presence: true
   validates :description, presence: true
   validates :size, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,11 @@ class User < ApplicationRecord
   has_many :purchases, dependent: :destroy
 
   validates :first_name, presence: true, on: :update
-  # validates :last_name, presence: true
+  validates :last_name, presence: true, on: :update
+  validates :photo_avatar, presence: true, on: :update
   # validates :alias, uniqueness: true, on: :update
   # validates :address, presence: true, if: :artist
-  # validates :photo_artists_banner, presence: true
+  validates :photo_artists_banner, presence: true, on: :update
   # validates :bank_account, uniqueness: true, presence: true
   # validates :bio, presence: true
   # validates :description, presence: true
@@ -20,7 +21,7 @@ class User < ApplicationRecord
   end
 
   def self.search(search)
-  where("first_name ILIKE ?", "%#{search}%")
+    where("first_name ILIKE ?", "%#{search}%")
   end
 
 end
